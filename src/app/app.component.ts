4e93d119d3429a46fa48fab1cfda31f40a63c929
Guilherme Ferreira <guilherme.fsilva23@gmail.com>
2024-01-14 15:21:55 -0300

    fetch: add app container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  results = [];
+  pushForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.pushForm = this.formBuilder.group({
+      hour: '',
+      shouldClosed: false
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.pushForm.value);
+  }
+
+  onClean(): void {
+    this.pushForm.reset();
+  }
 }
